5d65183b425cd66aa4195c5986706b4900040c00
m1000 <74680137+xParzivalx@users.noreply.github.com>
2024-10-20 04:08:36 +0200

    s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,9 @@ export default function RootLayout({
         },
       }}>
     <html lang="es" suppressHydrationWarning>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
