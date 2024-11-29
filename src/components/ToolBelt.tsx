ae4e58b9e4edc51f518b45560de1bbb7a13d6ca7
Marc Furriols <74680137+xm1000z@users.noreply.github.com>
2024-11-29 19:45:01 +0100

    a

--- src/components/ToolBelt.tsx
+++ src/components/ToolBelt.tsx
@@ -18,8 +18,7 @@ export const ToolBelt = () => {
           <Button
             onClick={handleChangeToolBeltType(ToolBeltType.TEXT)}
             variant={toolBeltType === ToolBeltType.TEXT ? "default" : "ghost"}
-            size={"xs"}
-            className="inline-flex items-center rounded-none justify-center whitespace-nowrap text-sm transition-colors py-2 px-3 dark:text-[#878787] text-primary dark:bg-[#2C2C2C] bg-gray-100 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:dark:bg-[#2C2C2C] data-[state=active]:bg-gray-100 data-[state=inactive]:dark:bg-[#1D1D1D] data-[state=inactive]:bg-white data-[state=inactive]:text-gray-600"
+            className="inline-flex items-center rounded-none justify-center whitespace-nowrap text-xs transition-colors py-2 px-3 dark:text-[#878787] text-primary dark:bg-[#2C2C2C] bg-gray-100 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:dark:bg-[#2C2C2C] data-[state=active]:bg-gray-100 data-[state=inactive]:dark:bg-[#1D1D1D] data-[state=inactive]:bg-white data-[state=inactive]:text-gray-600"
           >
             <TextIcon className="h-4 w-4 mr-2" />
             Texto
@@ -29,7 +28,6 @@ export const ToolBelt = () => {
           <Button
             onClick={handleChangeToolBeltType(ToolBeltType.IMAGE)}
             variant={toolBeltType === ToolBeltType.IMAGE ? "default" : "ghost"}
-            size={"xs"}
             className="inline-flex items-center rounded-none justify-center whitespace-nowrap text-sm transition-colors py-2 px-3 dark:text-[#878787] text-primary dark:bg-[#2C2C2C] bg-gray-100 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:dark:bg-[#2C2C2C] data-[state=active]:bg-gray-100 data-[state=inactive]:dark:bg-[#1D1D1D] data-[state=inactive]:bg-white data-[state=inactive]:text-gray-600"
           >
             <ImageIcon className="h-4 w-4 mr-2" />
@@ -42,7 +40,6 @@ export const ToolBelt = () => {
             variant={
               toolBeltType === ToolBeltType.DOCUMENT ? "default" : "ghost"
             }
-            size={"xs"}
             className="inline-flex items-center rounded-none justify-center whitespace-nowrap text-sm transition-colors py-2 px-3 dark:text-[#878787] text-primary dark:bg-[#2C2C2C] bg-gray-100 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:dark:bg-[#2C2C2C] data-[state=active]:bg-gray-100 data-[state=inactive]:dark:bg-[#1D1D1D] data-[state=inactive]:bg-white data-[state=inactive]:text-gray-600"
           >
             <FileIcon className="h-4 w-4 mr-2" />
