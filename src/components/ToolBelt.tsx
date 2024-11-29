1843dba42af4e54677388a4eaa7c51dd30cebadf
Marc Furriols <74680137+xm1000z@users.noreply.github.com>
2024-11-29 19:40:33 +0100

    aa

--- src/components/ToolBelt.tsx
+++ src/components/ToolBelt.tsx
@@ -13,12 +13,13 @@ export const ToolBelt = () => {
 
   return (
     <nav className="mb-4">
-      <ul className="flex items-center gap-2">
+      <ul className="flex items-center gap-2 inline-flex items-center justify-center bg-muted text-muted-foreground">
         <li>
           <Button
             onClick={handleChangeToolBeltType(ToolBeltType.TEXT)}
             variant={toolBeltType === ToolBeltType.TEXT ? "default" : "ghost"}
             size={"sm"}
+            className="inline-flex items-center justify-center whitespace-nowrap text-sm transition-colors py-2 px-3 dark:text-[#878787] text-primary dark:bg-[#2C2C2C] bg-gray-100 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:dark:bg-[#2C2C2C] data-[state=active]:bg-gray-100 data-[state=inactive]:dark:bg-[#1D1D1D] data-[state=inactive]:bg-white data-[state=inactive]:text-gray-600"
           >
             <TextIcon className="h-4 w-4 mr-2" />
             Texto
