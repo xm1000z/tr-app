bb5776e2f2d6df1916a88ee31b05cc98aa08725d
Marc Furriols <74680137+xm1000z@users.noreply.github.com>
2024-12-31 16:03:52 +0100

    a

--- src/components/TranslationBox.tsx
+++ src/components/TranslationBox.tsx
@@ -6,6 +6,7 @@ import { Textarea } from "./ui/textarea";
 import { TranslationBoxTypes } from "@/lib/constants";
 import { Button } from "./ui/button";
 import { textContext } from "@/providers";
+import { codeContext } from "@/providers/code/codeContext";
 
 interface TranslationBoxProps {
   type: TranslationBoxTypes;
@@ -17,16 +18,18 @@ export const TranslationBox: FC<TranslationBoxProps> = ({
   textareaProps,
 }) => {
   const { handleChangeTextToTranslate } = useContext(textContext);
+  const { handleSetCodeToTranslate } = useContext(codeContext);
 
   const value = textareaProps?.value?.toString().trim() ?? "";
 
   const handleCleanTextToTranslate = () => {
     handleChangeTextToTranslate("");
+    handleSetCodeToTranslate("");
   };
 
   return (
     <div className="flex flex-col gap-2 w-full border border-input bg-transparent px-3 py-2 text-sm shadow-sm">
-      <div className="flex gap-2 flex-1">
+      <div className="flex gap-2 flex-1 relative">
         <Textarea className="min-h-full" {...textareaProps} />
 
         {TranslationBoxTypes.SOURCE === type && value !== "" && (
@@ -34,6 +37,7 @@ export const TranslationBox: FC<TranslationBoxProps> = ({
             onClick={handleCleanTextToTranslate}
             variant="ghost"
             size="icon"
+            className="absolute top-2 right-2"
           >
             <Cross1Icon />
           </Button>
